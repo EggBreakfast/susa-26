047a124eebc720b890c160a8c2b0a6d72a7d021d
Egg <nicecream555@gmail.com>
2026-03-20 22:19:10 -0400

    save_system and save_data operational
    Both the SaveSystem (global) and Save_Data (class_name) are operational ! They use json files to create save data and the like. Nothing functional that actually impacts the game has been created yet, but it works! How exciting.

--- kitchen/ingredients/_ingredient/ingredient_data.gd
+++ kitchen/ingredients/_ingredient/ingredient_data.gd
@@ -9,17 +9,23 @@ extends Resource
 @export var overprep_percentage: float = 2.0
 
 @export_group ("Textures", "texture_")
+
 ##The texture to show when this ingredient has not yet been prepped.
 @export var texture_raw: Texture2D
+
 ##The texture to show when this ingredient has been prepped adequately.
 @export var texture_prepped: Texture2D
+
 ##The texture to show when this ingredient has been overprepped (burnt).
 @export var texture_overprepped: Texture2D
 
 @export_group ("Modulations", "modulate_")
+
 ##A "Modulate" for when the ingredient has not yet been prepped
 @export var modulate_raw: Color = Color.WHITE
+
 ##A "Modulate" for when the ingredient has been prepped
 @export var modulate_prepped: Color = Color.WHITE
+
 ##A "Modulate" for when the ingredient has been overprepped
 @export var modulate_overprepped: Color = Color.WHITE
---
+++ save_system/save_system.gd
@@ -0,0 +1,63 @@
+# implied class_name SaveSystem
+extends Node
+
+
+signal save_loaded(save_data: SaveData)
+signal save_saved(save_data: SaveData)
+
+
+## The directory to save and load data to/from. 
+const SAVE_DIRECTORY = "user://saves"
+
+
+## The saved file that is loaded (if any!)
+var current_save_data: SaveData
+
+
+@warning_ignore_start("unused_parameter")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"quick_save"):
+		save_data(1, SaveData.new())
+	
+	if event.is_action_pressed(&"quick_load"):
+		load_data(1)
+
+
+## Load a SaveData file into memory!
+func load_data(save_slot: int) -> void:
+	if not DirAccess.dir_exists_absolute(SAVE_DIRECTORY):
+		DirAccess.make_dir_recursive_absolute(SAVE_DIRECTORY)
+	
+	var filename: String = "%s/save-%s.json" % [ SAVE_DIRECTORY, save_slot]
+	var file_access: FileAccess = FileAccess.open(filename, FileAccess.READ)
+	var json: String = file_access.get_as_text()
+	current_save_data = SaveData.new()
+	current_save_data.from_json(json)
+	
+	save_loaded.emit(current_save_data)
+	
+	
+	# print_debug(current_save_data.current_round)
+	#print_debug("Loading from slot: ", save_slot)
+
+
+# Save the current SaveData to the disk. oh hoh 
+func save_data(save_slot: int, data: SaveData) -> void:
+	if not DirAccess.dir_exists_absolute(SAVE_DIRECTORY):
+		DirAccess.make_dir_recursive_absolute(SAVE_DIRECTORY)
+	
+	var filename: String = "%s/save-%s.json" % [ SAVE_DIRECTORY, save_slot]
+	var json: String = data.to_json()
+	var file_access: FileAccess = FileAccess.open(filename, FileAccess.WRITE)
+	file_access.store_string(json)
+	
+	save_saved.emit(current_save_data)
+	
+	#print_debug("Saving data to slot: ", data.to_json(), " | ", save_slot)
+
+
+
+# DirAccess: for folders and stuff! You can make new folders, read new folders...
+# DON'T SAVE YOUR DATA TO :res// !! Data is READ-ONLY. YOU CAN'T WRITE IN THERE. 
+# FileAccess: as the name implies
